ffd675f7f2408563d94d05304fdf1ec378e908e6
r4cky70 <rodrigoestrelar70@gmail.com>
2019-09-02 09:48:18 -0300

    .

--- ord_comparacao/src/main/java/sorting/divideAndConquer/MergeSort.java
+++ ord_comparacao/src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -12,7 +12,33 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		T[] aux = (T[]) new Comparable[array.length];
+		sort(array, leftIndex, rightIndex, aux);
+	}
+	public void sort(T[] array, int leftIndex, int rightIndex, T[] aux){
+		int mid = (leftIndex + rightIndex)/2;
+		if(rightIndex - leftIndex > 2){
+			sort(array, leftIndex, mid, aux);
+			sort(array, mid+1, rightIndex, aux);
+			merge(array, leftIndex, mid, rightIndex, aux);
+		}
+	}
+	public void merge(T[] array, int leftIndex, int mid, int rightIndex, T[] aux){
+		int p = leftIndex;
+		int m = mid;
+		while(leftIndex <= mid || m <= rightIndex){
+			if(leftIndex > mid){
+				aux[p++] = array[m++];
+			}else if(m > rightIndex){
+				aux[p++] = array[leftIndex++];
+			}else if(array[leftIndex].compareTo(array[m]) < 0){
+				aux[p++] = array[leftIndex++];
+			}else{
+				aux[p++] = array[m++];
+			}
+		}
+		for(int i = 0; i < array.length; ++i){
+			array[i] = aux[i];
+		}
 	}
 }
